f60406811bf2c14b8f0010fdf6fbd729c28a6367
Dieter Vercammen <dietervercammen93@gmail.com>
2017-12-08 22:58:04 +0100

    youtube thumbnails with url in console.log

--- master/public/javascripts/index.js
+++ master/public/javascripts/index.js
@@ -1,5 +1,13 @@
 function ytSearch() {
     $.get("http://localhost:3000/search/" + $('#youtube-search').val(), function(data, status){
-        console.log(data);
+        console.log(data.items);
+        $.each( data.items, function( key, value ) {
+            $video_id = data.items[key].id.videoId;
+            $video_thumb = data.items[key].snippet.thumbnails.high.url;
+
+            $( "<img src='" + $video_thumb + "' />" ).appendTo( ".thumbnail-wrapper" );
+            console.log("youtube url= https://www.youtube.com/watch?v="+ $video_id);
+            
+          });
     });
 }
