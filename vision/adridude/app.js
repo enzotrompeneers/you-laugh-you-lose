3acf49ba337abaa1528f659322caf14d9654269c
Adriaan Marain <adriaanmarain300@gmail.com>
2017-12-08 18:04:34 +0100

    vision create

---
+++ vision/adridude/app.js
@@ -0,0 +1,10 @@
+var vision = require('@google-cloud/vision');
+
+// Authenticating on a global basis.
+var projectId = 'you-laugh-you-lose-188416';
+
+var gcloud = require('google-cloud')({
+    projectId: projectId,
+    keyFilename: './key/You Laugh You Lose-daa1f93d2d75',
+    key: 'AIzaSyCC1u7i3UJkr0jeIDDseBxR_ggWdzaYTuc'
+});
