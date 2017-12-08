00ffeb64ac7c75e29f6fb8addbafe7e5ea8ff7f3
Yawuar Serna Delgado <yawuarsernadelgado@hotmail.be>
2017-12-09 00:52:30 +0100

    push latest changes

--- master/app.js
+++ master/app.js
@@ -44,14 +44,10 @@ io.on('socket', function(socket) {
   });
 });
 
-var line_history = [];
-io.on('connection', function (socket) {
-  for (var i in line_history) {
-    socket.emit('draw_line', { line: line_history[i] } );
-  }
-  socket.on('draw_line', function (data) {
-    line_history.push(data.line);
-    io.emit('draw_line', { line: data.line });
+// var line_history = [];
+io.on('socket', function (socket) {
+  socket.on('msg', function (data) {
+    socket.broadcast.emit('msg', msg);
  });
 });
 
