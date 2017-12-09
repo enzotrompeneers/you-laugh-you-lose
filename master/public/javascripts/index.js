2eb808f53d0f41ae435d3bb9718eb01205b415bc
Dieter Vercammen <dietervercammen93@gmail.com>
2017-12-09 01:39:30 +0100

    youtube iframe

--- master/public/javascripts/index.js
+++ master/public/javascripts/index.js
@@ -4,9 +4,26 @@ function ytSearch() {
         $.each( data.items, function( key, value ) {
             $video_id = data.items[key].id.videoId;
             $video_thumb = data.items[key].snippet.thumbnails.medium.url;
-
-            $( "<img src='" + $video_thumb + "' />" ).appendTo( ".thumbnail-wrapper" );
+            $( "<img class='"+ $video_id +"' src='" + $video_thumb + "' />" ).appendTo( ".thumbnail-wrapper" );
             console.log("youtube url= https://www.youtube.com/watch?v="+ $video_id + "?autoplay=1");
           });
     });
+
+    
+
+    
+
 }
+
+$(function() {
+    
+$( ".thumbnail-wrapper" ).on( "click", 'img', function() {
+    
+    var $video_id = $(this).attr('class');
+    $('.thumbnail-wrapper').hide();
+    $( "<iframe width='560' height='315' src='//www.youtube.com/embed/" + $video_id + "'frameborder='0' gesture='media' allow='encrypted-media' allowfullscreen</iframe>" ).appendTo( ".iframe-wrapper" );
+  });
+
+});
+
+
